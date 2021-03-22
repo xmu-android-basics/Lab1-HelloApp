e2eaa146f4514f238e083ebbdf7adb992edd81e3
吴德文@厦门大学 <windwood@xmu.edu.cn>
2021-03-22 15:20:54 +0800

    新建项目 + 设定项目名称为 HelloApp + 设定 package 为 io.github.xmu_android_basics.helloapp + 选择 minSdkVersion 为API 22 + 选择项目模板为 Empty Activity template

---
+++ app/src/main/java/io/github/xmu_android_basics/helloapp/MainActivity.java
@@ -0,0 +1,13 @@
+package io.github.xmu_android_basics.helloapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
