be9219b7befbd6a99fac65ab5fbea9ebc4a9a2f8
吴德文@厦门大学 <windwood@xmu.edu.cn>
2021-03-25 00:04:22 +0800

    实现按钮效果

--- app/src/main/java/io/github/xmu_android_basics/helloapp/MainActivity.java
+++ app/src/main/java/io/github/xmu_android_basics/helloapp/MainActivity.java
@@ -2,12 +2,28 @@ package io.github.xmu_android_basics.helloapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText personName;
+    TextView messageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        personName = findViewById(R.id.personName);
+        messageView = findViewById(R.id.welcomeMessage);
+    }
+
+    public void hello(View view) {
+        String messageTemplate = getString(R.string.welcomeMessage);
+        String message = String.format(messageTemplate, personName.getText().toString());
+
+        messageView.setText(message);
     }
 }
